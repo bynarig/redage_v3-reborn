f4540e34713720e33396ff2f539b6039100526f9
Yaroslav Shabelnyk <62596807+bynarig@users.noreply.github.com>
2025-02-28 00:54:37 +0100

    some clean && prod build

--- cef_new/vite.config.ts
+++ cef_new/vite.config.ts
@@ -1,13 +1,13 @@
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import * as path from "node:path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
+ build: {
+    outDir: path.resolve(__dirname, '../client_packages/interface'), // Output directory
+    // emptyOutDir: true, // Optional: Clears the output directory before building
   },
 });
